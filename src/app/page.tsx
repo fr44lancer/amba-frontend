9100164a0cf298f3fe7c581bc6ab3c05f5b03c85
Fr44lancer <sur@docus.ai>
2024-11-09 12:11:35 +0400

    Add clsx and framer-motion dependencies, update HomePageText component, and modify Tailwind config.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,7 @@ import img2 from '@/assets/img/hero/2.jpg';
 import img4 from '@/assets/img/hero/4.jpg';
 import img5 from '@/assets/img/hero/5.jpg';
 import FixedImage from '@/components/common/Image/FixedImage/FixedImage';
-import { HeroHighlight, Highlight } from '@/components/ui/hero-highlight';
-import { motion } from 'framer-motion';
+import { HomePageText } from '@/components/global/HomePageText';
 
 export const metadata: Metadata = {
   title: 'Amba',
@@ -17,39 +16,15 @@ export const metadata: Metadata = {
 
 export default async function HomePage() {
   return (
-    <>
-      <AppPage contentClassName={'no-padding'}>
-        <HeroHighlight>
-          <motion.h1
-            initial={{
-              opacity: 0,
-              y: 20,
-            }}
-            animate={{
-              opacity: 1,
-              y: [20, -5, 0],
-            }}
-            transition={{
-              duration: 0.5,
-              ease: [0.4, 0.0, 0.2, 1],
-            }}
-            className="text-2xl px-4 md:text-4xl lg:text-5xl font-bold text-neutral-700 dark:text-white max-w-4xl leading-relaxed lg:leading-snug text-center mx-auto "
-          >
-            With insomnia, nothing&apos;s real. Everything is far away.
-            Everything is a{' '}
-            <Highlight className="text-black dark:text-white">
-              պահեստային լուծումներ
-            </Highlight>
-          </motion.h1>
-        </HeroHighlight>
-        <CarouselSingleTestimonial>
-          <FixedImage src={img1.src} alt={''} width={700} height={500} />
-          <FixedImage src={img2.src} alt={''} width={700} height={500} />
-          <FixedImage src={img4.src} alt={''} width={700} height={500} />
-          <FixedImage src={img5.src} alt={''} width={700} height={500} />
-          <FixedImage src={img5.src} alt={''} width={700} height={500} />
-        </CarouselSingleTestimonial>
-      </AppPage>
-    </>
+    <AppPage contentClassName={'no-padding'}>
+      <HomePageText />
+      <CarouselSingleTestimonial>
+        <FixedImage src={img1.src} alt={''} width={700} height={500} />
+        <FixedImage src={img2.src} alt={''} width={700} height={500} />
+        <FixedImage src={img4.src} alt={''} width={700} height={500} />
+        <FixedImage src={img5.src} alt={''} width={700} height={500} />
+        <FixedImage src={img5.src} alt={''} width={700} height={500} />
+      </CarouselSingleTestimonial>
+    </AppPage>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,40 +1,45 @@
-import type { Config } from "tailwindcss"
+const svgToDataUri = require('mini-svg-data-uri');
+const {
+  default: flattenColorPalette,
+} = require('tailwindcss/lib/util/flattenColorPalette');
 
-const config = {
-  darkMode: ["class"],
-  content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-  ],
-  prefix: "",
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{ts,tsx}'],
+  darkMode: 'class',
   theme: {
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
-    },
     extend: {
-      keyframes: {
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
-        },
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-      },
+      // your config here
     },
   },
-  plugins: [require("tailwindcss-animate")],
-} satisfies Config
+  plugins: [
+    addVariablesForColors,
+    function ({ matchUtilities, theme }: any) {
+      matchUtilities(
+        {
+          'bg-dot-thick': (value: any) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="2.5"></circle></svg>`,
+            )}")`,
+          }),
+        },
+        {
+          values: flattenColorPalette(theme('backgroundColor')),
+          type: 'color',
+        },
+      );
+    },
+  ],
+};
 
-export default config
+// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme('colors'));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val]),
+  );
+
+  addBase({
+    ':root': newVars,
+  });
+}
